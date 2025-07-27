8759edd8d3bf8e5093bcd852ff63940ae13e76c3
Rishabh Tomar <rishabhtomar9999@gmail.com>
2025-07-27 17:26:12 +0530

    chunks update

--- RecipeMakerProject/vite.config.js
+++ RecipeMakerProject/vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true,
+      filename: 'dist/stats.html',
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
   resolve: {
     alias: {
       stream: 'stream-browserify',
@@ -23,5 +32,21 @@ export default defineConfig({
         }),
       ],
     },
+  },
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'react-vendor';
+            if (id.includes('firebase')) return 'firebase-vendor';
+            if (id.includes('lodash')) return 'lodash-vendor';
+            return 'vendor';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // optional: suppress warning if you're okay with it
   },
 });
