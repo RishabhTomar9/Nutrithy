5471fc29c7e2595b0524e88a3c776f75aa4f19ce
Rishabh Tomar <rishabhtomar9999@gmail.com>
2025-07-27 17:58:04 +0530

    .

--- RecipeMakerProject/src/main.jsx
+++ RecipeMakerProject/src/main.jsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './polyfills';
-import App from './App';
-
+import App from './App.jsx'
 import { Buffer } from 'buffer';
 import process from 'process';
 
